24f86a2b6ea5e09d17d0c18170a6bba697337dcc
Jakub Sarnowski <sarneeh@gmail.com>
2018-06-13 11:28:02 +0200

    refactor(example): remove subheader, make header customisable

--- example/src/components/header.js
+++ example/src/components/header.js
@@ -1,7 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export default styled.h1`
-  font-size: 2rem;
+const header = css`
   font-weight: 400;
   margin: 0 0 0.5rem 0;
   display: inline-block;
@@ -9,4 +8,23 @@ export default styled.h1`
   + a button {
     margin: 0 0 0.5rem 0.5rem;
   }
+
+  &:last-child {
+    margin: 0;
+  }
+`;
+
+export const H1 = styled.h1`
+  ${header};
+  font-size: 2rem;
+`;
+
+export const H2 = styled.h2`
+  ${header};
+  font-size: 1.2rem;
+`;
+
+export const H3 = styled.h3`
+  ${header};
+  font-size: 1rem;
 `;
