5e439f9a06d486da3e8aa8663a95d0b270442e72
Jakub Sarnowski <hello@jsarnowski.com>
2022-03-10 10:04:52 +0100

    chore: remove babel

--- example/src/containers/index.tsx
+++ example/src/containers/index.tsx
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import { hot } from 'react-hot-loader';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import { theme } from '../config';
