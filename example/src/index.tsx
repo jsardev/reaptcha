5553cfc67bcc26d2bef47e9f9787c0856bd6512b
John Gozde <john@gozde.ca>
2022-06-13 17:57:47 -0600

    chore: use createRoot, StrictMode in example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './containers';
 
-render(<App />, document.getElementById('app'));
+const app = document.getElementById('app');
+if (!app) {
+  throw new Error('No app element found');
+}
+
+const root = createRoot(app);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
